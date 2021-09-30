18d5707425272df75a6f1afdee506e39863319c8
r4ge <71139938+r4gesingh47@users.noreply.github.com>
2021-09-30 23:40:07 +0530

    Add files via upload
    initial files

---
+++ app.js
@@ -0,0 +1,26 @@
+const category=document.getElementById('choose-category');
+var val="test";
+category.addEventListener('change', function() {
+    val=category.value;
+    console.log(val);  
+    var elements=document.getElementsByClassName("question");
+    if(val!="all"){
+    for(var i=0;i<elements.length;i++)
+    {
+        elements[i].classList.remove('show');
+        elements[i].classList.add('default');
+    }
+    elements=document.getElementsByClassName(val);
+    for(var i=0;i<elements.length;i++)
+    {
+        elements[i].classList.add('show');
+    }
+    }
+    else{
+        for(var i=0;i<elements.length;i++)
+    {
+        elements[i].classList.add('show');
+    }
+    }
+});
+
